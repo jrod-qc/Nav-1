3255bd87060ed38dd223311cd997b771f55e44f4
Juan Rodriguez - Work <juanrodriguez-work@Jrod-3.local>
2021-02-17 09:29:31 -0800

    update

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-$(".request-quote-btn").on("click", function() {
-    $(".modal-content").css("display", "flex");
+$(".lp-request-quote-btn").on("click", function() {
+    $(".lp-modal-content").css("display", "flex");
 });
 
-$(".modal-exit-btn").on("click", function() {
-    $(".modal-content").css("display", "none");
+$(".lp-modal-exit-btn").on("click", function() {
+    $(".lp-modal-content").css("display", "none");
 });
 
-$(".video-modal-btn").on("click", function() {
-    $(".modal-video").css("display", "flex");
+$(".lp-video-modal-btn").on("click", function() {
+    $(".lp-modal-video").css("display", "flex");
 });
 
-$(".video-modal-exit-btn-icon").on("click", function() {
-    $(".modal-video").css("display", "none");
+$(".lp-video-modal-exit-btn-icon").on("click", function() {
+    $(".lp-modal-video").css("display", "none");
 });
 
-$(".burger-btn").on("click", function() {
-    $(".nav-mobile").css("transform", "translateX(0px)");
+$(".lp-burger-btn").on("click", function() {
+    $(".lp-nav-mobile").css("transform", "translateX(0px)");
 });
 
-$(".mobile-exit-nav").on("click", function() {
-    $(".nav-mobile").css("transform", "translateX(250px)");
+$(".lp-mobile-exit-nav").on("click", function() {
+    $(".lp-nav-mobile").css("transform", "translateX(250px)");
 });
 
-$(".anchor-clicked").on("click", function() {
-    $(".nav-mobile").css("transform", "translateX(250px)");
+$(".lp-anchor-clicked").on("click", function() {
+    $(".lp-nav-mobile").css("transform", "translateX(250px)");
 });
